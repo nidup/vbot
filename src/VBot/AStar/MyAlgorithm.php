14562ac116cd891e578544ffd54022bd9a1678fc
Nicolas Dupont <nicolas@akeneo.com>
2014-08-26 22:05:18 +0200

    Avoid to inherit from Algorithm to prepare to get rid of NodeList

--- src/VBot/AStar/MyAlgorithm.php
+++ src/VBot/AStar/MyAlgorithm.php
@@ -3,18 +3,22 @@
 namespace VBot\AStar;
 
 use JMGQ\AStar\Node;
+use JMGQ\AStar\NodeList;
 use JMGQ\AStar\Algorithm;
 
 // Quick attempt from git@github.com:jmgq/php-a-star.git
 // Not use callback algorithm due to extensible use of call user func
-class MyAlgorithm extends Algorithm
+class MyAlgorithm
 {
+    private $openList;
+    private $closedList;
     private $terrainCost;
 
     public function __construct(TerrainCost $terrainCost)
     {
+        $this->openList = new NodeList();
+        $this->closedList = new NodeList();
         $this->terrainCost = $terrainCost;
-        parent::__construct();
     }
 
     /**
@@ -71,5 +75,112 @@ class MyAlgorithm extends Algorithm
         $euclideanDistance = sqrt($rowFactor + $columnFactor);
 
         return $euclideanDistance;
+    }
+
+    /**
+     * @return NodeList
+     */
+    public function getOpenList()
+    {
+        return $this->openList;
+    }
+
+    /**
+     * @return NodeList
+     */
+    public function getClosedList()
+    {
+        return $this->closedList;
+    }
+
+    /**
+     * Sets the algorithm to its initial state
+     */
+    public function clear()
+    {
+        $this->getOpenList()->clear();
+        $this->getClosedList()->clear();
+    }
+
+    /**
+     * @param Node $start
+     * @param Node $goal
+     * @return Node[]
+     */
+    public function run(Node $start, Node $goal)
+    {
+        $path = array();
+
+        $this->clear();
+
+        $start->setG(0);
+        $start->setH($this->calculateEstimatedCost($start, $goal));
+
+        $this->getOpenList()->add($start);
+
+        while (!$this->getOpenList()->isEmpty()) {
+            $currentNode = $this->getOpenList()->extractBest();
+
+            $this->getClosedList()->add($currentNode);
+
+            if ($currentNode->getID() === $goal->getID()) {
+                $path = $this->generatePathFromStartNodeTo($currentNode);
+                break;
+            }
+
+            $successors = $this->computeAdjacentNodes($currentNode, $goal);
+
+            foreach ($successors as $successor) {
+                if ($this->getOpenList()->contains($successor)) {
+                    $successorInOpenList = $this->getOpenList()->get($successor);
+
+                    if ($successor->getG() >= $successorInOpenList->getG()) {
+                        continue;
+                    }
+                }
+
+                if ($this->getClosedList()->contains($successor)) {
+                    $successorInClosedList = $this->getClosedList()->get($successor);
+
+                    if ($successor->getG() >= $successorInClosedList->getG()) {
+                        continue;
+                    }
+                }
+
+                $this->getClosedList()->remove($successor);
+
+                $this->getOpenList()->add($successor);
+            }
+        }
+
+        return $path;
+    }
+
+    private function generatePathFromStartNodeTo(Node $node)
+    {
+        $path = array();
+
+        $currentNode = $node;
+
+        while ($currentNode !== null) {
+            array_unshift($path, $currentNode);
+
+            $currentNode = $currentNode->getParent();
+        }
+
+        return $path;
+    }
+
+    private function computeAdjacentNodes(Node $node, Node $goal)
+    {
+        $nodes = $this->generateAdjacentNodes($node);
+
+        foreach ($nodes as $adjacentNode) {
+            $adjacentNode->setParent($node);
+            $adjacentNode->setG($node->getG() + $this->calculateRealCost($node, $adjacentNode));
+            $adjacentNode->setH($this->calculateEstimatedCost($adjacentNode, $goal));
+        }
+
+        return $nodes;
     }
 }
