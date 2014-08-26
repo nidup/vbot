2ade10141aec564d4f6a53561f9ed47abc4c5f44
Nicolas Dupont <nicolas@akeneo.com>
2014-08-26 19:48:28 +0200

    Add an xhprof option in the run command

--- src/VBot/Command/RunTrainingCommand.php
+++ src/VBot/Command/RunTrainingCommand.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 use VBot\Client\Client;
 use VBot\Bot\BotFactory;
@@ -27,7 +28,8 @@ class RunTrainingCommand extends Command
             ->setDescription('Run a Vindinium training')
             ->addArgument('key', InputArgument::REQUIRED, 'Secret user key')
             ->addArgument('turns', InputArgument::REQUIRED, 'Number of turns')
-            ->addArgument('map', InputArgument::REQUIRED, 'Map to use (m1, m2, m3, m4, m5, m6)');
+            ->addArgument('map', InputArgument::REQUIRED, 'Map to use (m1, m2, m3, m4, m5, m6)')
+            ->addOption('xhprof', null, InputOption::VALUE_NONE, 'Enable profiling with xhprof');
     }
 
     /**
@@ -35,6 +37,10 @@ class RunTrainingCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if ($input->getOption('xhprof')) {
+            xhprof_enable();
+        }
+
         $output->writeln('Run a Vindinium training');
         $output->writeln('');
 
@@ -53,5 +59,15 @@ class RunTrainingCommand extends Command
             $input->getArgument('map')
         );
         $client->load();
+
+        if ($input->getOption('xhprof')) {
+            $xhprofData = xhprof_disable();
+            $XHPROF_ROOT = realpath('/usr/local/xhprof-0.9.4');
+            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
+            include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
+            $xhprofRuns = new \XHProfRuns_Default();
+            $runId = $xhprofRuns->save_run($xhprofData, "xhprof_vbot");
+            echo PHP_EOL.'xhprof run '.$runId.PHP_EOL;
+        }
     }
 }
